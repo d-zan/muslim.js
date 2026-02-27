b9c7d908b1515596526e19eb0c0258f6d2913c7f
Yousef Karm <143213837+d-zan@users.noreply.github.com>
2026-02-28 01:05:11 +0200

    Add Prayer.json data file with prayer times and locations

--- Function/converttoAMPM.js
+++ Function/converttoAMPM.js
@@ -1,28 +1,28 @@
 function Time(timings) {
-  let [hours,minutes] = timings.split(":").map(Number);
-      let period = hours >= 12 ? 'PM' : 'AM';
-      hours = hours % 12 || 12;
-      return `${hours}:${minutes.toString().padStart(2, '0')} ${period}`
+  let [hours, minutes] = timings.split(":").map(Number);
+  let period = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+  return `${hours}:${minutes.toString().padStart(2, "0")} ${period}`;
 }
 function converttoAMPM(data) {
-    if (!data) return;
-   
-      return {
-        fajr:Time(data.Fajr),
-        sunrise:Time(data.Sunrise),
-        dhuhr:Time(data.Dhuhr),
-        asr:Time(data.Asr),
-        maghrib:Time(data.Maghrib),
-        isha:Time(data.Isha),
-        imsak:Time(data.Imsak),
-        midnight:Time(data.Midnight),
-        firstthird:Time(data.Firstthird),
-        lastthird:Time(data.Lastthird)
-      };
-    }
+  if (!data) return;
+
+  return {
+    fajr: Time(data.Fajr),
+    sunrise: Time(data.Sunrise),
+    dhuhr: Time(data.Dhuhr),
+    asr: Time(data.Asr),
+    maghrib: Time(data.Maghrib),
+    isha: Time(data.Isha),
+    imsak: Time(data.Imsak),
+    midnight: Time(data.Midnight),
+    firstthird: Time(data.Firstthird),
+    lastthird: Time(data.Lastthird),
+  };
+}
 function convertOneToAMPM(data) {
-    if (!data) return;
-   
-      return Time(data);
-    }
-module.exports = {converttoAMPM,convertOneToAMPM};
+  if (!data) return;
+
+  return Time(data);
+}
+module.exports = { converttoAMPM, convertOneToAMPM };
