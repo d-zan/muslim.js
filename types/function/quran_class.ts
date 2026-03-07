7405dc78bee595709689f6e78844b07059407272
Yousef Karm <143213837+d-zan@users.noreply.github.com>
2026-03-07 04:06:56 +0200

    feat: Expand QuranTextNames types with additional language support and update interface structure

--- types/function/quran_class.ts
+++ types/function/quran_class.ts
@@ -1,4 +1,3 @@
-import { TextEdition,QuranIdentifierText,QuranFullText,SurahNames,SurahText,QuranAdvancedOptions } from './../quran';
 type ar_QuranTextNames = 
 | "تفسير المیسر"
 | "القرآن الكريم المبسط (تشكيل بسيط) (simple)"
@@ -20,18 +19,82 @@ type ar_QuranTextNames =
 | "تنوير المقباس من تفسير بن عباس"
 | "الـتـفـسـيـر الـوسـيـط"
 | "تفسير البغوي";
-export interface QuranTextNamesByLanguage {
+
+type az_QuranTextNames = 
+| "Məmmədəliyev & Bünyadov"
+| "Musayev";
+type bn_QuranTextNames = 
+| "মুহিউদ্দীন খান"
+| "জহুরুল হক";
+type cs_QuranTextNames = 
+| "Hrbek"
+| "Nykl";
+type de_QuranTextNames = 
+| "Abu Rida"
+| "Bubenheim & Elyas"
+| "Khoury"
+| "Zaidan";
+type dv_QuranTextNames = 
+| "ދިވެހި";
+type en_QuranTextNames = 
+| "Ahmed Ali"
+| "Ahmed Raza Khan"
+| "Arberry"
+| "Asad"
+| "Daryabadi"
+| "Hilali & Khan"
+| "Pickthall"
+| "Qaribullah & Darwish"
+| "Saheeh International"
+| "Sarwar"
+| "Yusuf Ali"
+| "Maududi"
+| "Shakir"
+| "Transliteration"
+| "Clear Qur'an - Talal Itani"
+| "Mubarakpuri"
+| "Qarai"
+| "Wahiduddin Khan";
+
+type fa_QuranTextNames = 
+| "آیتی"
+| "فولادوند"
+| "الهی قمشه‌ای"
+| "مکارم شیرازی"
+| "انصاریان"
+| "بهرام پور"
+| "خرمشاهی"
+| "مجتبوی"
+| "خرمدل"
+| "معزی"
+| "قرائتی"
+| "صادقی تهرانی"
+| "صفوی";
+type fr_QuranTextNames = 
+| "Hamidullah";
+
+type ha_QuranTextNames = 
+| "Gumi"
+type hi_QuranTextNames = 
+| "फ़ारूक़ ख़ान & नदवी"
+| "फ़ारूक़ ख़ान & अहमद"
+
+
+
+
+/** QuranTextNames */
+interface qtnMap {
    "ar":ar_QuranTextNames;
-   "az":""
-   "bn":""
-   "cs":""
-   "de":""
-   "dv":""
-   "en":""
-   "fa":""
-   "fr":""
-   "ha":""
-   "hi":""
+   "az":az_QuranTextNames;
+   "bn":bn_QuranTextNames;
+   "cs":cs_QuranTextNames;
+   "de":de_QuranTextNames;
+   "dv":dv_QuranTextNames;
+   "en":en_QuranTextNames;
+   "fa":fa_QuranTextNames;
+   "fr":fr_QuranTextNames;
+   "ha":ha_QuranTextNames;
+   "hi":hi_QuranTextNames;
    "id":""
    "it":""
    "ja":""
@@ -71,3 +134,55 @@ export interface QuranTextNamesByLanguage {
    "ce":""
 }
 
+
+
+export interface QuranTextNamesByLanguage {
+   "ar":qtnMap['ar'];
+   "az":qtnMap['az'];
+   "bn":qtnMap['bn'];
+   "cs":qtnMap['cs'];
+   "de":qtnMap['de'];
+   "dv":qtnMap['dv'];
+   "en":qtnMap['en'];
+   "fa":qtnMap['fa'];
+   "fr":qtnMap['fr'];
+   "ha":qtnMap['ha'];
+   "hi":qtnMap['hi'];
+   "id":qtnMap['id'];
+   "it":qtnMap['it'];
+   "ja":qtnMap['ja'];
+   "ko":qtnMap['ko'];
+   "ku":qtnMap['ku'];
+   "ml":qtnMap['ml'];
+   "nl":qtnMap['nl'];
+   "no":qtnMap['no'];
+   "pl":qtnMap['pl'];
+   "pt":qtnMap['pt'];
+   "ro":qtnMap['ro'];
+   "ru":qtnMap['ru'];
+   "sd":qtnMap['sd'];
+   "so":qtnMap['so'];
+   "sq":qtnMap['sq'];
+   "sv":qtnMap['sv'];
+   "sw":qtnMap['sw'];
+   "ta":qtnMap['ta'];
+   "tg":qtnMap['tg'];
+   "th":qtnMap['th'];
+   "tr":qtnMap['tr'];
+   "tt":qtnMap['tt'];
+   "ug":qtnMap['ug'];
+   "ur":qtnMap['ur'];
+   "uz":qtnMap['uz'];
+   "es":qtnMap['es'];
+   "bg":qtnMap['bg'];
+   "bs":qtnMap['bs'];
+   "ms":qtnMap['ms'];
+   "zh":qtnMap['zh'];
+   "si":qtnMap['si'];
+   "ba":qtnMap['ba'];
+   "my":qtnMap['my'];
+   "am":qtnMap['am'];
+   "ber":qtnMap['ber'];
+   "ps":qtnMap['ps'];
+   "ce":qtnMap['ce'];
+}
